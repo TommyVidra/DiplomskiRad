4afb2f4f0e63124501d959815ef5cfbda8e80ebc
tomislav.vidovic <tomislav.vidovic@fer.hr>
2022-11-09 22:57:59 +0100

    Add class for notifications, add advanced search option

--- Rad/flaskr/static/js/test.js
+++ Rad/flaskr/static/js/test.js
@@ -3,7 +3,22 @@ const LIGHT_COLOR = "#fff"
 let SEARCH_TYPE = 1
 let ADVANCED_SEARCH = false
 let ADVANCED_SET = false
-let ACTIVE_BUTTONS = []
+let ACTIVE_BUTTONS = {}
+
+const NONCOMPLEX_ATTRS = ["autor_upisa", "prostor", "datum", "vrijeme", "zamjena_sudionika", "sudionik1", "sudionik2", "situacijski_kontekst"]
+
+const EXVOCATION_GREETING = "exv_poz"
+const INVOCATION_GREETING = "inv_poz"
+const EXVOCATION_RESPONSE = "exv_odz"
+const INVOCAITON_RESPONSE = "inv_odz"
+
+const REST_ATTR = "ostalo"
+const REST_ATTRS = {
+    inv_poz : ["opis_ipv_ostalo", "opis_ipn_ostalo"],
+    exv_odz : ["opis_eon_ostalo", "opis_eov_ostalo"], 
+    inv_odz : ["opis_iov_ostalo", "opis_ion_ostalo"], 
+    exv_poz : ["opis_epn_ostalo", "opis_epv_ostalo"]
+}
 
 $("#inv-exv").on("click", () => {
     if($("#inv-exv").text() === "Invokacija"){
@@ -26,26 +41,85 @@ $.ajax({
     localStorage.setItem("data", JSON.stringify(data))
 })
 
-function attributeSearch(j, id){
+function searchNonComplexAttr(event, keys){
+    for(key in keys){
+        if(typeof event[key] !== 'undefined'){
+            const inputValue = $(`${attrName}-input`).val()
+            if(event[key].includes(inputValue))
+                return true
+        }
+    }
+    return false
+}
+
+function attributeSearch(j){
     let keys = []
     let returnEntries = {}
+    let nonComplex = true
+
     // Treba napraviti da ide od kljuca do kljuca i trazi podate iz j
     for (button in ACTIVE_BUTTONS){
-        keys.push(button.split("-button")[0])
-    }
-    for(key in keys){
-        if(key === "exv_odz" || key === "exv_poz" || key === "inv_odz" || key === "inv_poz"){
-            for(subAttribute in j[id][key]){
-                if(subAttribute === key){
-                    if($(`${key}-input`).val() === j[id][key][subAttribute])
-                        returnEntries[id] = j[id]                    
-                }
-            } 
+        if(ACTIVE_BUTTONS[button]){
+            key = button.split("-button")[0]
+            if(!NONCOMPLEX_ATTRS.includes(key))
+                nonComplex = false
+            keys.push(key)
         }
-        else{
-            if($(`${key}-input`).val() === j[id][key])
-                returnEntries[id] = j[id]               
+    }
+    
+    for(key in j){
+        let matches = true
+        let restMatches = false
+        let restNeeded = false
+        
+        for(index in keys){
+            const attr = keys[index]
+            const inputValue = $(`#${attr}-input`).val()
+            if(NONCOMPLEX_ATTRS.includes(attr)){
+                if(typeof j[key][attr] === 'undefined' || !j[key][attr].includes(inputValue))
+                    matches = false          
             }
+            else{
+                if(attr === REST_ATTR){
+                    restNeeded = true
+                    for(restKey in REST_ATTRS){
+                        for(index in REST_ATTRS[restKey]){
+                            restAttr = REST_ATTRS[restKey][index]
+                            if(typeof j[key][restKey] !== 'undefined' && typeof j[key][restKey][restAttr] !== 'undefined' && j[key][restKey][restAttr].includes(inputValue))
+                                restMatches = true
+                        }
+                    }
+                }
+                else{
+                    const attrId = attr.split("_")[1]
+                    const exvocation = attrId.includes("e")
+                    const greeting = attrId.includes("p")
+                    if(exvocation){
+                        if(greeting){
+                            if(typeof j[key][EXVOCATION_GREETING] === 'undefined' || typeof j[key][EXVOCATION_GREETING][attr] === 'undefined' || !j[key][EXVOCATION_GREETING][attr].includes(inputValue))
+                                matches = false 
+                        }
+                        else{
+                            if(typeof j[key][EXVOCATION_RESPONSE] === 'undefined' || typeof j[key][EXVOCATION_RESPONSE][attr] === 'undefined' || !j[key][EXVOCATION_RESPONSE][attr].includes(inputValue))
+                                matches = false 
+                        }
+                    }
+                    else{
+                        if(greeting){
+                            if(typeof j[key][INVOCATION_GREETING] === 'undefined' || typeof j[key][INVOCATION_GREETING][attr] === 'undefined' || !j[key][INVOCATION_GREETING][attr].includes(inputValue))
+                                matches = false 
+                        }
+                        else{
+                            if(typeof j[key][INVOCAITON_RESPONSE] === 'undefined' || typeof j[key][INVOCAITON_RESPONSE][attr] === 'undefined' || !j[key][INVOCAITON_RESPONSE][attr].includes(inputValue))
+                                matches = false 
+                        }
+                    }
+                }
+                
+            }
+        }
+        if(matches && ((restNeeded && restMatches) || (!restMatches & !restNeeded)))
+            returnEntries[key] = j[key]
     }
     return returnEntries
 }
@@ -174,7 +248,6 @@ function addInvocations(j, key, searchData){
         ipvDescription = ""
 
         if(invocationGreeting["pozdrav"].includes(searchData) || searchData === ""){
-            console.log("koji kurac")
             attrString = ""
             restString = ""
             rest = '<p class="card-text"><span>ostalo</span>'
@@ -434,23 +507,16 @@ function addListeners(j, key){
 
 function search(){
     const searchData = $("#main-search-input").val()
-    console.log(searchData)
     let j = JSON.parse(JSON.parse(localStorage["data"]))
     let k = {}
     $("#dictionary-content").contents().remove()
     if(ADVANCED_SEARCH){
-        for(key in j){
-        k = attributeSearch(j, key)
-        }
-        console.log(k)
+        k = attributeSearch(j)
         j = k
-        console.log(j)
     }
     let index = 0
     for(key in j){
-        console.log(key)
         let html = addInvocations(j, key, searchData)
-        console.log(html)
         if(html !== ""){
             if (index%2 === 0){
                 index += 1
@@ -458,13 +524,27 @@ function search(){
                 addListeners(j, key)
             }
             else{
-                console.log
                 $(`#row_${index}`).append(`<div class="col-sm-6"><div class="card bg-light mb-3" id="${key}">${html}</div></div>`)
                 addListeners(j, key)
                 index += 1
             }
         }
-} 
+    } 
+    let notify = new Notification()
+    if(index === 0){
+          notify.addNotification({
+            type: "error",
+            title: "Greška",
+            message: "Navedeni parametri ne postoje u riječniku!"
+          });
+    }
+    else{
+        notify.addNotification({
+            type: "success",
+            title: "Uspjeh",
+            message: `Pronađeno je ${index} zapisa!`
+        });
+    }
 }
 
 $("#main-search-input").on('keypress',function(e) {
@@ -475,16 +555,17 @@ $("#main-search-input").on('keypress',function(e) {
 });
 
 function addButtonListener(id){
+    ACTIVE_BUTTONS[id] = false
     $(`#${id}`).on('click', () =>{
-        if(ACTIVE_BUTTONS.includes(id)){
+        if(ACTIVE_BUTTONS[id]){
             $(`#${id}`).css("background-color", LIGHT_COLOR)
             $(`#${id}`).css("color", DARK_COLOR)
-            ACTIVE_BUTTONS.pop(id)
+            ACTIVE_BUTTONS[id] = false
         }
         else{
             $(`#${id}`).css("background-color", DARK_COLOR)
             $(`#${id}`).css("color", LIGHT_COLOR)
-            ACTIVE_BUTTONS.push(id)
+            ACTIVE_BUTTONS[id] = true
         }
 
     })
